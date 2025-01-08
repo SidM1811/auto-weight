d33693b6dc70f1ca0d0bbda523c9cf88815b1d5b
Siddhant Mukherjee <sidm1811@mit.edu>
2025-01-08 05:27:09 +0000

    Fixed teacher data collection

--- src/combined.py
+++ src/combined.py
@@ -62,7 +62,9 @@ class Args:
     """the surrogate clipping coefficient"""
     clip_vloss: bool = True
     """Toggles whether or not to use a clipped loss for the value function, as per the paper."""
-    ent_coef: float = 0.01
+    student_ent_coef: float = 0.01
+    """coefficient of the entropy"""
+    teacher_ent_coef: float = 0.00
     """coefficient of the entropy"""
     vf_coef: float = 0.5
     """coefficient of the value function"""
@@ -102,10 +104,15 @@ def make_env_id(env_id, idx, capture_video, run_name):
 
     return thunk
 
+# def sample_teacher_params(num_envs):
+#     random_weights = torch.rand(num_envs, args.num_shaping) * 2.0
+#     default_shaping = torch.tensor([100.0, 100.0, 100.0, 10.0, 0.3, 0.03]).repeat(num_envs, 1)
+#     return default_shaping * random_weights
+
 def sample_teacher_params(num_envs):
-    random_weights = torch.rand(num_envs, args.num_shaping) * 2.0
-    default_shaping = torch.tensor([100.0, 100.0, 100.0, 10.0, 0.3, 0.03]).repeat(num_envs, 1)
-    return default_shaping * random_weights
+    random_weights = torch.rand(num_envs, args.num_shaping)
+    random_weights /= random_weights.sum(dim=1, keepdim=True)
+    return random_weights
 
 if __name__ == "__main__":
     args_json = json.load(open('config/combined_config.json'))
@@ -364,18 +371,26 @@ if __name__ == "__main__":
                         studentv_loss = 0.5 * ((studentnewvalue - b_student_returns[mb_student_inds]) ** 2).mean()
 
                     studententropy_loss = studententropy.mean()
-                    loss = studentpg_loss - args.ent_coef * studententropy_loss + studentv_loss * args.vf_coef
+                    loss = studentpg_loss - args.student_ent_coef * studententropy_loss + studentv_loss * args.vf_coef
                 else:
                     loss = 0.0
                     
                 if len(mb_teacher_inds) > 1:
-                    
+                        
                     _, studentlogprob, studententropy, studentnewvalue = student_agent.get_action_and_value(b_teacher_obs[mb_teacher_inds], action = b_teacher_actions.long()[mb_teacher_inds])
                     _, teacherlogprob, teacherentropy, teachernewextvalue, teachernewintvalue = teacher_agent.get_action_and_value(b_teacher_obs[mb_teacher_inds], action = b_teacher_actions.long()[mb_teacher_inds], params = b_teacher_params[mb_teacher_inds])
                     teachernewvalue = teachernewextvalue
                     
-                    studentratio = (studentlogprob - b_teacher_logprobs[mb_teacher_inds]).exp()
-                    teacherratio = (teacherlogprob - b_teacher_logprobs[mb_teacher_inds]).exp()
+                    studentlogratio = (studentlogprob - b_teacher_logprobs[mb_teacher_inds])
+                    studentratio = studentlogratio.exp()
+                    teacherlogratio = (teacherlogprob - b_teacher_logprobs[mb_teacher_inds])
+                    teacherratio = teacherlogratio.exp()
+                    
+                    with torch.no_grad():
+                        # calculate approx_kl http://joschu.net/blog/kl-approx.html
+                        old_approx_kl = (-teacherlogratio).mean()
+                        approx_kl = ((teacherratio - 1) - teacherlogratio).mean()
+                        clipfracs += [((teacherratio - 1.0).abs() > args.clip_coef).float().mean().item()]
                     
                     mb_teacher_advantages = b_teacher_advantages[mb_teacher_inds]
                     mb_teacher_shaped_advantages = b_teacher_shaped_advantages[mb_teacher_inds]
@@ -424,10 +439,11 @@ if __name__ == "__main__":
                         teacherintv_loss = 0.5 * ((teachernewintvalue - b_teacher_shaped_returns[mb_teacher_inds]) ** 2).mean()
                         
                     teacherentropy_loss = teacherentropy.mean()
-                    loss += teacherpg_loss - args.ent_coef * teacherentropy_loss + (teacherv_loss + teacherintv_loss) * args.vf_coef
+                    loss += teacherpg_loss - args.teacher_ent_coef * teacherentropy_loss + (teacherv_loss + teacherintv_loss) * args.vf_coef
                     
                     # Importance sampling loss
                     
+                    # TODO: This is problematic!!
                     mb_gen_weight = state_generator.param_ratio(b_teacher_obs[mb_teacher_inds], b_teacher_params[mb_teacher_inds])
                     mb_norm_gen_weight = mb_gen_weight.softmax(dim=1)
                     
@@ -458,16 +474,20 @@ if __name__ == "__main__":
                 torch.nn.utils.clip_grad_norm_(teacher_agent.parameters(), args.max_grad_norm)
                 student_optimizer.step()
                 teacher_optimizer.step()
+                
+        y_pred, y_true = b_teacher_values.cpu().numpy(), b_teacher_returns.cpu().numpy()
+        var_y = np.var(y_true)
+        explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_pred) / var_y
 
         # # TRY NOT TO MODIFY: record rewards for plotting purposes
         writer.add_scalar("charts/learning_rate", teacher_optimizer.param_groups[0]["lr"], global_step)
-        writer.add_scalar("losses/value_loss", teacherv_loss.item(), global_step)
-        writer.add_scalar("losses/policy_loss", teacherpg_loss.item(), global_step)
-        # writer.add_scalar("losses/entropy", entropy_loss.item(), global_step)
-        # writer.add_scalar("losses/old_approx_kl", old_approx_kl.item(), global_step)
-        # writer.add_scalar("losses/approx_kl", approx_kl.item(), global_step)
-        writer.add_scalar("losses/clipfrac", np.mean(clipfracs), global_step)
-        # writer.add_scalar("losses/explained_variance", explained_var, global_step)
+        writer.add_scalar("losses/teacher_value_loss", teacherv_loss.item(), global_step)
+        writer.add_scalar("losses/teacher_policy_loss", teacherpg_loss.item(), global_step)
+        writer.add_scalar("losses/teacher_entropy", teacherentropy_loss.item(), global_step)
+        writer.add_scalar("losses/tecaher_old_approx_kl", old_approx_kl.item(), global_step)
+        writer.add_scalar("losses/tecaher_approx_kl", approx_kl.item(), global_step)
+        writer.add_scalar("losses/teacher_clipfrac", np.mean(clipfracs), global_step)
+        writer.add_scalar("losses/teacher_explained_variance", explained_var, global_step)
         print("SPS:", int(global_step / (time.time() - start_time)))
         writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)), global_step)
         
